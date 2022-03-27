727a4a23d7cca6f92ae08f2d53d76178af4fe992
Vadimerr <vadimzherebcov03@gmail.com>
2022-03-27 21:06:20 +0300

    start

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  bool f = true; 
+  float a = 0;
+
+  std::cout << "Hi !!!\n";
+  std::cin >> a;
+  std::cout << " a = " << a << "\n" << std::endl;
+  return 0;
+}
